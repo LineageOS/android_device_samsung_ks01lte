d895186aa73088862fac8e3c6433dd390f8828fd
Vitalii Kulikov <solk@solk.org.ua>
2017-01-17 00:14:25 +0100

    ks01lte: Set INITIAL_COPYRIGHT_YEAR
    Change-Id: I6ee1ecc767f44cf431f0c179922b643383d519c9

--- extract-files.sh
+++ extract-files.sh
@@ -21,6 +21,8 @@ set -e
 DEVICE=samsung
 VENDOR=ks01lte
 
+export INITIAL_COPYRIGHT_YEAR=2014
+
 # Load extractutils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
